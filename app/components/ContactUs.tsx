484d9f82a6fba7a0d4ae90c4738c5e1d5a34a75c
KotteJR <aleksandar.kotevski@adamass.se>
2025-03-01 17:38:57 +0100

    aslkdj

--- app/components/ContactUs.tsx
+++ app/components/ContactUs.tsx
@@ -5,52 +5,52 @@ import {
   Text,
   Textarea,
   TextInput,
-  Title,
-} from '@mantine/core';
-import { ContactIconsList } from './ContactIcons';
-import classes from '../styles/ContactUs.module.scss';
+} from "@mantine/core";
+import { ContactIconsList } from "./ContactIcons";
+import classes from "../styles/ContactUs.module.scss";
 
 export function ContactUs() {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.contactWrapper} id="contact-us"> {/* Added id for scrolling */}
-        <SimpleGrid cols={{ base: 1, sm: 2 }} spacing={50}>
-          <div>
-            <Text className={classes.description} mt="xs" mb={20}>
-              Please contact us using the form or the contact details below.
-            </Text>
+    <section className={classes.contactSection}> {/* Added wrapper for full height */}
+      <div className={classes.wrapper}>
+        <div className={classes.contactWrapper} id="contact-us">
+          <SimpleGrid cols={{ base: 1, sm: 2 }} spacing={50}>
+            <div>
+              <Text className={classes.description} mt="xs" mb={20}>
+                Please contact us using the form or the contact details below.
+              </Text>
+              <ContactIconsList />
+            </div>
 
-            <ContactIconsList />
-          </div>
+            <div className={classes.form}>
+              <TextInput
+                label="Email"
+                placeholder="your@email.com"
+                required
+                classNames={{ input: classes.input, label: classes.inputLabel }}
+              />
+              <TextInput
+                label="Name"
+                placeholder="John Doe"
+                mt="md"
+                classNames={{ input: classes.input, label: classes.inputLabel }}
+              />
+              <Textarea
+                required
+                label="Your message"
+                placeholder="Anything and Everything"
+                minRows={4}
+                mt="md"
+                classNames={{ input: classes.input, label: classes.inputLabel }}
+              />
 
-          <div className={classes.form}>
-            <TextInput
-              label="Email"
-              placeholder="your@email.com"
-              required
-              classNames={{ input: classes.input, label: classes.inputLabel }}
-            />
-            <TextInput
-              label="Name"
-              placeholder="John Doe"
-              mt="md"
-              classNames={{ input: classes.input, label: classes.inputLabel }}
-            />
-            <Textarea
-              required
-              label="Your message"
-              placeholder="Anything and Everything"
-              minRows={4}
-              mt="md"
-              classNames={{ input: classes.input, label: classes.inputLabel }}
-            />
-
-            <Group justify="flex-end" mt="md">
-              <Button className={classes.control}>Send message</Button>
-            </Group>
-          </div>
-        </SimpleGrid>
+              <Group justify="flex-end" mt="md">
+                <Button className={classes.control}>Send message</Button>
+              </Group>
+            </div>
+          </SimpleGrid>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
