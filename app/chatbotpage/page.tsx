bc27b7d338529fc9291805aadb3eb6e1d42c9776
KotteJR <aleksandar.kotevski@adamass.se>
2025-03-21 08:04:17 +0100

    aslkdj

--- app/chatbotpage/page.tsx
+++ app/chatbotpage/page.tsx
@@ -1,13 +1,2 @@
-"use client";
 
-import { Chatbot } from "./Chatbot";
-import { FooterCentered } from "../LandingPage/components/FooterCentered";
-
-export default function ChatbotPage() {
-    return (
-        <>
-            <Chatbot />
-            <FooterCentered />
-        </>
-    );
 }
