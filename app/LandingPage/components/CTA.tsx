42b24d94022850bb5cfb44f3cd675ba3b03a0871
KotteJR <aleksandar.kotevski@adamass.se>
2025-03-21 16:07:58 +0100

    aslkdj

--- app/LandingPage/components/CTA.tsx
+++ app/LandingPage/components/CTA.tsx
@@ -1,18 +1,20 @@
-import { Button, Container, Text, Title } from "@mantine/core";
+import { Button, Text, Title } from "@mantine/core";
 import classes from "../styles/CTA.module.scss";
 
 export function CTA() {
   return (
     <div className={classes.heroCTAContainer}>
-      <Container className={classes.heroCTACard}>
-        <Title order={2} className={classes.heroCTATitle}>
-        Accelerate Your Growth Today        </Title>
-        <Text size="lg" className={classes.heroCTAText}>
-        Partner with Adamass to unlock strategic insights, optimize operations, and drive success.        </Text>
+      <div className={classes.heroCTACard}>
+        <Title className={classes.title}>Accelerate Your Growth Today</Title>
+
+        <Text c="dimmed" size="lg" className={classes.heroCTAtext}>
+          Partner with Adamass to unlock strategic insights, optimize operations, and drive success.
+        </Text>
+
         <div className={classes.heroCTAButtons}>
           <Button variant="default" className={classes.learnMoreButton}>Contact Us</Button>
         </div>
-      </Container>
+      </div>
     </div>
   );
-} 
+}
